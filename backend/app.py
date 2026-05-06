8e3092d9af7d9b1b2dc691acfc06b4524973b8e0
Erik Ramich <erik.ramich@gmail.com>
2026-05-06 15:22:34 +0200

    write upload mask and get res from backend

--- backend/app.py
+++ backend/app.py
@@ -1,11 +1,14 @@
-from flask import Flask, request, jsonify
+from flask import Flask, request, send_file
 from flask_cors import CORS
 import numpy as np
 import cv2
+import io
 
 app = Flask(__name__)
-CORS(app)
-
+CORS(app, origins="http://localhost:4200")
+@app.route("/")
+def home():
+    return "Backend is running 🚀"
 @app.route("/upload", methods=["POST"])
 def upload():
     file = request.files["image"]
@@ -16,8 +19,9 @@ def upload():
     colored = cv2.applyColorMap(img, cv2.COLORMAP_JET)
 
     _, buffer = cv2.imencode(".jpg", colored)
+    io_buf = io.BytesIO(buffer)
 
-    return buffer.tobytes(), 200, {"Content-Type": "image/jpeg"}
+    return send_file(io_buf, mimetype="image/jpeg")
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000)
--- how-to-docker.md
+++ how-to-docker.md
@@ -8,8 +8,17 @@
     docker compose down
 
 ## rebuild only when needed 
-    docker compose up
+    docker compose up   
 
+## one liner to flush everything and rebuild app
+    docker compose down --volumes --remove-orphans && docker compose build --no-cache && docker compose up
+
+## clear cache
+    docker compose down -v
+    docker rm -f $(docker ps -aq)
+    docker system prune -a -f
+    
 ## or just run everything with this command in bash
     chmod +x run.sh
     ./run.sh
+
